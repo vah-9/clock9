8f3990da6bc03162140f9d8e6e41388bed145e2c
Ivan Zhukhovitskiy <driveshuft@gmail.com>
2025-01-30 13:45:49 +0300

    Added settings saving for different layouts

--- app/src/main/java/com/example/clock9/MainActivity.java
+++ app/src/main/java/com/example/clock9/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.clock9;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextClock;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
+
+    SettingsData settingsData; // настройки приложения
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -27,37 +38,88 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        InitializeComponents();
+        initializeSettings();
 
     }
 
-    private void InitializeComponents(){
-        InitializeClock();
-    }
+    private void initializeSettings(){
 
-    private void InitializeClock(){
-        TextClock textClock = findViewById(R.id.textClock);
-        textClock.setFormat12Hour(null);
-
-        float scaleX;
-        float scaleY;
-        String formatString;
-
-        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            formatString = "HH\nmm\nss";
-            scaleY = 1f;
-            scaleX = 1.5f;
-        } else {
-            formatString = "HH:mm:ss";
-            scaleY = 1.5f;
-            scaleX = 1f;
+        settingsData = new SettingsData();
+        File file = new File(getFilesDir(), SettingsData.settingsDataFileName);
+        // считываем из файла настройки
+        try (FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            settingsData = (SettingsData) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e){
+            // если не удалось прочитать файл, ставим параметр по умолчанию
+            Log.d("ReadError",e.toString());
+            int font_size_default_integer = getResources().getInteger(R.integer.font_size_default_integer);
+            settingsData.font_size_land = font_size_default_integer;
+            settingsData.font_size_port = font_size_default_integer;
         }
-        textClock.setFormat24Hour(formatString);
-        textClock.setScaleY(scaleY);
-        textClock.setScaleX(scaleX);
-        textClock.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 144);
+        updateClock();
     }
 
+    public void onScreenTap(View v){
+        // прячем или показываем кнопки, при нажатии на экран
+        invertVisibility(R.id.btIncrease);
+        invertVisibility(R.id.btDecrease);
+        invertVisibility(R.id.btSave);
+    }
+
+    private void invertVisibility(int id){
+        View obj = findViewById(id);
+        if (obj.getVisibility() == Button.VISIBLE){
+            obj.setVisibility(Button.INVISIBLE);
+        }else{
+            obj.setVisibility(Button.VISIBLE);
+        }
+    }
+
+
+    public void btSave_onClick(View v) {
+
+        try (FileOutputStream outputStream = openFileOutput(SettingsData.settingsDataFileName, Context.MODE_PRIVATE);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
+            // сереализуем и сохраняем в файл настройки
+            objectOutputStream.writeObject(settingsData);
+            objectOutputStream.flush();
+        }
+        catch (IOException e) {
+            Log.d("WriteError",e.toString());
+        }
+    }
+
+    public void btIncrease_onClick(View v){
+        // увеличиваем соответствующий размер шрифта
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            settingsData.font_size_port += 2;
+        }else{
+            settingsData.font_size_land += 2;
+        }
+        updateClock();
+    }
+
+    public void btDecrease_onClick(View v){
+        // уменьшаем соответствующий размер шрифта
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            settingsData.font_size_port -= 2;
+        }else{
+            settingsData.font_size_land -= 2;
+        }
+        updateClock();
+    }
+
+    private void updateClock(){
+
+        TextClock textClock = findViewById(R.id.textClock);
+        // устанавливаем размер шрифта из настроек, в зависимости от ориентации экрана
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            textClock.setTextSize(TypedValue.COMPLEX_UNIT_DIP, settingsData.font_size_port);
+        }else{
+            textClock.setTextSize(TypedValue.COMPLEX_UNIT_DIP, settingsData.font_size_land);
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/example/clock9/SettingsData.java
@@ -0,0 +1,13 @@
+package com.example.clock9;
+
+
+import java.io.Serializable;
+
+public class SettingsData implements Serializable {
+    // сериализуемый класс для хранения настроек приложения
+    // можно было хранить их в хэшмапе, но я уже реализовал этот класс, мб переделаю
+    public static String settingsDataFileName = "settingsData.json";
+    public int font_size_port;
+    public int font_size_land;
+
+}
